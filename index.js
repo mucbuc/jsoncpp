73aca6e1db41b0d90cc4959e28580b41eb33502d
mbusenitz <mbusenitz@gmail.com>
2015-12-02 21:24:12 -0800

    fix test2

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 var traverse = require( 'traverjs' )
-  , util = require( 'util' );
+  , Promise = require( 'promise' );
 
 function processJSON(json, name) {
 
-    var result = {
-            "null": [],
-            "boolean": [],
-            number: [],
-            string: [],
-            object: [],
-            array: []
-          };
+    return new Promise( function(resolve, reject) {
 
-    traverse( json, function(o, next) {
-        var name = Object.keys(o)[0]
-          , value = o[name]
-          , type = typeof value;
-        
-        if (type == 'object') {
-            if (Array.isArray(value)) {
-                type = 'array';
-            } 
-            else if (value == null) {
-                type = 'null';
+
+        var result = {
+                "null": [],
+                "boolean": [],
+                number: [],
+                string: [],
+                object: [],
+                array: []
+              };
+
+        traverse( json, function(o, next) {
+            var name = Object.keys(o)[0]
+              , value = o[name]
+              , type = typeof value;
+            
+            if (type == 'object') {
+                if (Array.isArray(value)) {
+                    type = 'array';
+                } 
+                else if (value == null) {
+                    type = 'null';
+                }
             }
-        }
-        result[type].push({name: name, value: value});
-        next();
-    } )
-    .then( function() {
-        console.log( util.inspect( result ) );
-	t.end();
+            result[type].push({name: name, value: value});
+            next();
+        } )
+        .then( function() {
+            resolve(result);
+        })
+        .catch( reject );
     });
 }
 
--- test2/test.js
+++ test2/test.js
@@ -3,11 +3,17 @@
 var test = require( 'tape' )
   , Expector = require( 'expector' ).SeqExpector
   , fs = require( 'fs' )
+  , util = require( 'util' )
   , processJSON = require( '../index' );
 
 test( 'basic', function(t) {
 	fs.readFile( './data.json', function(err, data) {
 		if (err) throw err;
-		processJSON( JSON.parse( data.toString() ) );
+		processJSON( JSON.parse( data.toString() ) )
+		.then( function(result) {
+			console.log( util.inspect(result) );
+			t.pass();
+        	t.end();
+		});
 	});
 });
