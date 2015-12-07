57538bc2d1a08bbd8108aa2352639c0dd3b0bd74
mucbuc <mbusenitz@gmail.com>
2015-12-06 21:10:16 -0800

    writer

--- bin/write_cpp.js
+++ bin/write_cpp.js
@@ -2,67 +2,11 @@ var assert = require( 'assert' )
   , util = require( 'util' )
   , traverse = require( 'traverjs' )
   , Promise = require( 'promise' )
-  , processJSON = require( './process_json' );
+  , processJSON = require( './process_json' )
+  , Writer = require( './writer' );
 
-assert( typeof processJSON ); 
-
-function Writer(tabInit)
-{
-  var tabCount = 0
-    , instance = this
-    , init = '';
-
-  this.open = function() {
-    ++tabCount;
-  };
-
-  this.close = function() {
-    --tabCount;
-  };
-
-  this.write = function( text ) {
-    return instance.tabs(tabCount) + text.replace( /\n/g,  instance.tabs(tabCount) );
-  };
-
-  this.defineTemplateClassBegin = function( template, name ) {
-    var result = init + 'template ' + template;
-    init = '\n';
-    result += instance.defineStructBegin( name );
-    return result;
-  };
-
-  this.defineStructBegin = function( name ) {
-    var result = instance.write( 'struct ' + name );
-    result += instance.tabs(tabCount++) + '{';
-    return result;
-  };
-
-  this.defineStructEnd = function( name ) {
-    assert( tabCount );
-    return instance.tabs(--tabCount) + '};';
-  };
-
-  this.includeGuardBegin = function() {
-    return '';
-  };
-
-  this.includeGuardEnd = function() {
-    return '';
-  };
-
-  this.mangle = function(source) {
-    return '_' + source;
-  };
-
-  this.tabs = function(count) {
-    var result = init;
-    init = '\n';
-    while (count--) {
-      result += '  ';
-    }
-    return result;
-  };
-}
+assert( typeof processJSON !== 'undefined' ); 
+assert( typeof Writer !== 'undefined' );
 
 function writeCPP( json, name ) {
   return new Promise( function(resolve, reject) { 
@@ -92,60 +36,51 @@ function writeCPPInternal( json, name ) {
       , content = ''
       , members = [];
 
-    //content += writer.includeGuardBegin();
-    //content += writer.defineTemplateClassBegin( '<T = std::string, U = int>', name );
-    
-    // content += writer.write( 'typedef T string_type;' );
-    // content += writer.write( 'typedef U number_type;' );
-
     traverse( json, function(type, nextType) { 
       
-
-
       var key = Object.keys( type )[0];
       var value = type[key];
       
       switch (key) 
       {
-        case "object": 
-          traverse( value, function( object, nextObject ) {
-            
-            processJSON(object.value)
-            .then( function(result) {
-              var typeName = mapType( object.name );
-              writeCPPInternal( result, typeName )
-              .then( function(nested) {
-                content += writer.defineStructBegin( typeName ); 
-                content += writer.write(nested);
-                content += writer.defineStructEnd();
-                content += writer.write(typeName + ' ' + writer.mangle( object.name ) + ' = {};' );
-                members.push( object.name ); 
-                nextObject(); 
-              } );
-            });
-          })
-          .then( nextType )
-          .catch( nextType );
-          break;
+      case "object": 
+        traverse( value, function( object, nextObject ) {
+          
+          processJSON(object.value)
+          .then( function(result) {
+            var typeName = mapType( object.name );
+            writeCPPInternal( result, typeName )
+            .then( function(nested) {
+              content += writer.defineStructBegin( typeName ); 
+              content += writer.write(nested);
+              content += writer.defineStructEnd();
+              content += writer.write(typeName + ' ' + writer.mangle( object.name ) + ' = {};' );
+              members.push( object.name ); 
+              nextObject(); 
+            } );
+          });
+        })
+        .then( nextType )
+        .catch( nextType );
+        break;
+      case "array": 
+        traverse( value, function( array, nextArray) {
 
-        case "array": 
-          traverse( value, function( array, nextArray) {
-
-            var types = [];
-            traverse( array, function(type, nextType) {
-              types.push( mapType(typeof key) );
-              nextType();
-            })
-            .then( function() {
-              content += writer.write( 'std::tuple<' + types.join(', ') 
-                 + '> ' + writer.mangle( array.name ) 
-                 + ' = {' + util.inspect( array.value).slice(1,-1) + '};' );
-              members.push( array.name ); 
-              nextArray();
-            });
+          var types = [];
+          traverse( array, function(type, nextType) {
+            types.push( mapType(typeof key) );
+            nextType();
           })
-          .then( nextType );
-          break;
+          .then( function() {
+            content += writer.write( 'std::tuple<' + types.join(', ') 
+               + '> ' + writer.mangle( array.name ) 
+               + ' = {' + util.inspect( array.value).slice(1,-1) + '};' );
+            members.push( array.name ); 
+            nextArray();
+          });
+        })
+        .then( nextType );
+        break;
       case "string":
       case "number":
       case "boolean":
---
+++ bin/writer.js
@@ -0,0 +1,61 @@
+var assert = require( 'assert' )
+
+function Writer(tabInit)
+{
+  var tabCount = 0
+    , instance = this
+    , init = '';
+
+  this.open = function() {
+    ++tabCount;
+  };
+
+  this.close = function() {
+    --tabCount;
+  };
+
+  this.write = function( text ) {
+    return instance.tabs(tabCount) + text.replace( /\n/g,  instance.tabs(tabCount) );
+  };
+
+  this.defineTemplateClassBegin = function( template, name ) {
+    var result = init + 'template ' + template;
+    init = '\n';
+    result += instance.defineStructBegin( name );
+    return result;
+  };
+
+  this.defineStructBegin = function( name ) {
+    var result = instance.write( 'struct ' + name );
+    result += instance.tabs(tabCount++) + '{';
+    return result;
+  };
+
+  this.defineStructEnd = function( name ) {
+    assert( tabCount );
+    return instance.tabs(--tabCount) + '};';
+  };
+
+  this.includeGuardBegin = function() {
+    return '';
+  };
+
+  this.includeGuardEnd = function() {
+    return '';
+  };
+
+  this.mangle = function(source) {
+    return '_' + source;
+  };
+
+  this.tabs = function(count) {
+    var result = init;
+    init = '\n';
+    while (count--) {
+      result += '  ';
+    }
+    return result;
+  };
+}
+
+module.exports = Writer;
