de71011183e7f2227348f49f0f91c34cc7a4b770
mbusenitz <mbusenitz@gmail.com>
2016-01-18 05:53:42 -0800

    array test"

--- test/index.js
+++ test/index.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
 var Expector = require( 'expector' ).Expector
-  , cp = require( 'child_process' )
   , test = require( 'tape' )
   , fs = require( 'fs' )
   , translate = require( '../index.js' );
 
-test( 'smoke', function(t) {
-  var expector = new Expector(t)
-    , child;
+test.skip( 'smoke', function(t) {
+  var expector = new Expector(t);
 
   fs.readFile( 'test/src/sample.h', function(err, data) {
     if (err) throw err;
@@ -24,5 +22,26 @@ test( 'smoke', function(t) {
       expector.emit( result.toString() );
       expector.check();
     } );
+  });
+});
+
+test.only( 'array', function(t) {
+
+  var expector = new Expector(t);
+
+  fs.readFile( 'test/src/result_array.h', function(err, data) {
+    if (err) throw err;
+    expector.expect( data.toString() );
+
+    translate( 'test/array.json', function(result) {
+      result = result.toString();
+      for (var i = 0; i < 2; ++i) 
+      {
+        result = result.substring( result.indexOf('\n') + 1 );
+      }
+
+      expector.emit( result.toString() );
+      expector.check();
+    });
   });
 });
